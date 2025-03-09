6f603c72fbe6106e7e6e4b8212e5407943b83cca
SimpleImplementations <mati.mussel+study@gmail.com>
2025-03-09 04:52:32 -0300

    use poller in CandlestickChartPage

--- src/pages/CandlestickChartPage.tsx
+++ src/pages/CandlestickChartPage.tsx
@@ -1,54 +1,19 @@
-import { useState, useEffect } from "react";
-import { MarketData } from "../interfaces/apiModels";
 import CandlestickChart from "../components/CandlestickChart";
+import { useMarketDataFromPoller } from "../hooks/use-market-data-from-poller";
+import MarketDataPoller from "../components/MarketDataPoller";
 
 function CandlestickChartPage() {
-  const [marketDataArray, setMarketDataArray] = useState<MarketData[]>([]);
-  const [loading, setLoading] = useState(false);
-
-  const fetchData = async () => {
-    setLoading(true);
-    try {
-      const response = await fetch("http://localhost:8002/api/market_data/mock_broker_always_new_data");
-      const json = await response.json();
-      setMarketDataArray((prevData) => [...prevData, json as MarketData]);
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    // Fetch initial data when component mounts
-    fetchData();
-  }, []);
-
-  // Render loading state
-  if (loading && marketDataArray.length === 0) {
-    return <div>Loading chart data...</div>;
-  }
-
-  // Render empty state
-  if (marketDataArray.length === 0) {
-    return <div>No market data available. Try refreshing.</div>;
-  }
+  const [marketDataArray, handleDataReceived] = useMarketDataFromPoller();
 
   return (
     <div>
       <h1>Candlestick Chart</h1>
 
-      <div className="button-container">
-        <button onClick={fetchData} disabled={loading}>
-          {loading ? "Loading..." : "Fetch More Data"}
-        </button>
-      </div>
+      <MarketDataPoller onDataReceived={handleDataReceived} />
 
       <div>
-        {marketDataArray.length === 0 && !loading ? (
+        {marketDataArray.length === 0 ? (
           <p>No data available. Click "Fetch More Data" to load market data.</p>
-        ) : loading && marketDataArray.length === 0 ? (
-          <p>Loading chart data...</p>
         ) : (
           <CandlestickChart data={marketDataArray} />
         )}
