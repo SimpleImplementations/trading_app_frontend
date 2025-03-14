4e17ae2f3bd7028642b949b04c28ca49dc6f32cc
SimpleImplementations <mati.mussel+study@gmail.com>
2025-03-14 17:38:59 -0300

    add new variable to execution config

--- src/interfaces/executionConfig.ts
+++ src/interfaces/executionConfig.ts
@@ -2,10 +2,12 @@ export interface ExecutionConfig {
   readonly apiBaseUrl: string;
   readonly brokerType: string;
   readonly pollingIntervalMs: number;
+  readonly strategy: string;
 }
 
 export const defaultConfig: ExecutionConfig = {
   apiBaseUrl: 'http://localhost:8002/api',
   brokerType: 'mock_broker_always_new_data',
-  pollingIntervalMs: 5000
+  pollingIntervalMs: 5000,
+  strategy: 'ema_crossover'
 };
